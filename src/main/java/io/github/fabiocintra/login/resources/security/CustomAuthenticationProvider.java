ee83a7e13120c15f3bad467d428b200142be9ee8
Fabio Henrique Silva Cintra <fh076417@gmail.com>
2026-05-02 18:18:39 -0300

    feat: I added security to the application, and the customization the Authentication

---
+++ src/main/java/io/github/fabiocintra/login/resources/security/CustomAuthenticationProvider.java
@@ -0,0 +1,42 @@
+package io.github.fabiocintra.login.resources.security;
+
+import io.github.fabiocintra.login.resources.entity.User;
+import io.github.fabiocintra.login.resources.service.UserService;
+import io.github.fabiocintra.login.utils.exceptions.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserService userService;
+
+    @Override
+    public @Nullable Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String login = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        User userFinded = userService.searchByUserName(login);
+        if(userFinded == null){
+            throw new UserNotFoundException("User/Password not exists");
+        }
+
+        if(!password.equals(userFinded.getPassword())){
+            throw new UserNotFoundException("User/Password not exists");
+        }
+
+        return new CustomAuthentication(userFinded);
+
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.isAssignableFrom(UsernamePasswordAuthenticationToken.class);
+    }
+}
---
+++ src/main/java/io/github/fabiocintra/login/resources/security/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package io.github.fabiocintra.login.resources.security;
+
+import org.apache.tomcat.util.http.Method;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true,jsr250Enabled = true)
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests( authorize -> {
+                    authorize.requestMatchers(HttpMethod.POST, "/users/**").permitAll();
+                    authorize.anyRequest().authenticated();
+                })
+                .build();
+    }
+
+}
